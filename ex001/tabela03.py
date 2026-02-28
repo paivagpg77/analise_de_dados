dac81e79fe75b4579c2c6a829d9de40ef7b71b8f
paivagpg77 <paivag171@gmail.com>
2026-02-28 09:50:56 -0300

    interpretacao de grafico com biblioteca

---
+++ ex001/tabela03.py
@@ -0,0 +1,6 @@
+import plotly.express as px
+import pandas as pd
+
+tabela = pd.read_csv('ClientesBanco.csv')
+grafico = px.histogram(tabela , x = 'Contatos 12m' , color  = 'Categoria')
+grafico.show()
